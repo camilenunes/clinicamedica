47df8af6473a511bde90e6a38e39d2a8f2335324
sofiagabriele <sofia_hein@estudante.sesisenai.org.br>
2025-11-26 14:58:11 -0300

    add: mudando para aparência do mac

--- sistema-gerenciador-clinica-medica/src/main/java/com/aati/scm/Main.java
+++ sistema-gerenciador-clinica-medica/src/main/java/com/aati/scm/Main.java
@@ -3,12 +3,19 @@ package com.aati.scm;
 import java.sql.SQLException;
 
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import com.aati.scm.view.TelaPrincipal;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedLookAndFeelException {
+
+        UIManager.setLookAndFeel(new FlatMacLightLaf());
+
         SwingUtilities.invokeLater(() -> {
+
            
             try {
                 TelaPrincipal tela = new TelaPrincipal();
